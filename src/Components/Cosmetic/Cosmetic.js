dc2b83df2292296852de18176f3ce2bdc3a9f750
Shahinur04 <shahinur04042002@gmail.com>
2023-03-26 17:37:52 +0600

    json fake jenerator data

--- src/Components/Cosmetic/Cosmetic.js
+++ src/Components/Cosmetic/Cosmetic.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 
 const Cosmetic = (props) => {
-    const{name,price,id}=props.cosmetic;
+    const{name,age,id}=props.cosmetic;
     // console.log(props)
     return (
         <div>
             <h2>buy:{name}</h2>
-            <p>price;{price}</p>
+            <p>Age;{age}</p>
             <h3>id:{id}</h3>
         </div>
     );
--- src/Components/Cosmetics/Cosmetics.js
+++ src/Components/Cosmetics/Cosmetics.js
@@ -4,12 +4,37 @@ import Cosmetic from '../Cosmetic/Cosmetic';
 import './Cosmetics.css'
 const Cosmetics = () => {
    const cosmetics=[
-    {name:'alta',id:1,price:200},
-    {name:'blta',id:2,price:300},
-    {name:'mlta',id:3,price:400},
-    {name:'klta',id:4,price:500},
-    {name:'jlta',id:5,price:100}
-   ]
+    {
+      "id": "64202e08de2554a2de0b517a",
+      "age": 33,
+      "name": "Harris Jordan"
+    },
+    {
+      "id": "64202e081995867e1e81b45f",
+      "age": 34,
+      "name": "Herring Roman"
+    },
+    {
+      "id": "64202e08d8416b90a196547c",
+      "age": 20,
+      "name": "Cook Gonzales"
+    },
+    {
+      "id": "64202e08a957fcc6788d896f",
+      "age": 20,
+      "name": "Sexton Chapman"
+    },
+    {
+      "id": "64202e08b6eeeb426719d4ae",
+      "age": 24,
+      "name": "Ina Hutchinson"
+    },
+    {
+      "id": "64202e088c01ca1496b7ab45",
+      "age": 25,
+      "name": "Carver Byers"
+    }
+  ]
     return (
         <div className='container'>
             <h1>Welcome to my cosmetics shop</h1>
